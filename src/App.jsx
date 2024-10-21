5c2af196d666669cfe5b617fa14dfb5625ce9f11
Jihed Berlim <jihedberlim@gmail.com>
2024-10-21 15:30:49 -0300

    feat: create Header component,  module.css, and create global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+
+import './global.css'
+
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Header />
+      <Post 
+        author="Jihed Berlim" 
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti eius quis dolore eligendi iste molestiae. Corporis a reprehenderit nihil, aliquam nesciunt quaerat. Similique odio asperiores ad reiciendis ipsam vel nesciunt."
+      />
+      <Post 
+        author="Victor Eduardo" 
+        content="Other content"
+      />
+    </div>
   )
 }
 
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react' // Pacote responsável pelo core do React, onde há as funcionalidades que são compartilhadas entre todas as interfaces
 import { createRoot } from 'react-dom/client' // Integração do core do React com a DOM (Document Object Model => representação do HTML através do JS)
-import App from './App.jsx'
+import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
