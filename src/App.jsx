f81b0ef606b3c54035342737eda27f29097f1297
Jihed Berlim <jihedberlim@gmail.com>
2024-10-22 14:21:51 -0300

    feat: create a css module for the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from './App.module.css'
+
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Jihed Berlim" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti eius quis dolore eligendi iste molestiae. Corporis a reprehenderit nihil, aliquam nesciunt quaerat. Similique odio asperiores ad reiciendis ipsam vel nesciunt."
-      />
-      <Post 
-        author="Victor Eduardo" 
-        content="Other content"
-      />
+
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post
+            author="Jihed Berlim"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti eius quis dolore eligendi iste molestiae. Corporis a reprehenderit nihil, aliquam nesciunt quaerat. Similique odio asperiores ad reiciendis ipsam vel nesciunt."
+          />
+          <Post
+            author="Victor Eduardo"
+            content="Other content"
+          />
+        </main>
+      </div>
     </div>
   )
 }
